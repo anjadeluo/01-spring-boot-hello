790b757b6a44766532631908977c5eecbf5a2a50
luoanyu <511399131@qq.com>
2018-10-24 01:16:01 +0800

    create project

---
+++ src/main/java/com/bjsxt/controller/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.bjsxt.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * SpringBoot HelloWorld
+ * @author Administrator
+ *
+ */
+@Controller
+public class HelloWorld {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public Map<String, Object> showHelloWorld(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("msg", "HelloWorld");
+		return map;
+	}
+
+	@RequestMapping("/hello2")
+	@ResponseBody
+	public Map<String, Object> showHelloWorld2(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("msg", "HelloWorld");
+		return map;
+	}
+}
